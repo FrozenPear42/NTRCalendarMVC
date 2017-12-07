1a49f97a91b7085d46abf682746b7b413f906d95
Wojciech Gruszka <wojciech@gruszka.eu>
2017-12-07 11:33:09 +0100

    Add project files.

---
+++ NTRCalendarMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NTRCalendarMVC.Startup))]
+namespace NTRCalendarMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
