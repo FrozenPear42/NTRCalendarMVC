016aee8e8f4e48b39f9dffb52a38d02f776105e1
Wojciech Gruszka <wojciech@gruszka.eu>
2017-12-19 16:31:53 +0100

    upgraded bootstrap

--- NTRCalendarMVC/Models/Attendance.cs
+++ NTRCalendarMVC/Models/Attendance.cs
@@ -9,13 +9,14 @@ namespace NTRCalendarMVC
     [Table("Attendance")]
     public partial class Attendance
     {
+        [Key]
+        public Guid AttendanceID { get; set; }
+
         public Guid AppointmentID { get; set; }
 
         public Guid PersonID { get; set; }
 
         public bool Accepted { get; set; }
-
-        public Guid AttendanceID { get; set; }
 
         [Column(TypeName = "timestamp")]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
