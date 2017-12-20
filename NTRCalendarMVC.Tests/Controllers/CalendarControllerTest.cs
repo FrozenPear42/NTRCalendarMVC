aa9553322046184a4c2e2b11c01922069e26b2f4
Wojciech Gruszka <wojciech@gruszka.eu>
2017-12-21 00:49:02 +0100

    some shitty tests

--- NTRCalendarMVC.Tests/Controllers/CalendarControllerTest.cs
+++ NTRCalendarMVC.Tests/Controllers/CalendarControllerTest.cs
@@ -3,12 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NTRCalendarMVC.Controllers;
+using NTRCalendarMVC.ViewModels;
 
-namespace NTRCalendarMVC.Tests.Controllers
-{
+namespace NTRCalendarMVC.Tests.Controllers {
     [TestClass]
-    class CalendarControllerTest
-    {
+    public class CalendarControllerTest {
+        [TestMethod]
+        public void PrevShouldPrev() {
+            var controller = new CalendarController();
+            var today = DateTime.Today;
+
+            var result = (RedirectToRouteResult) controller.Prev(today);
+
+            Assert.AreEqual(result.RouteValues["FirstDay"], today.AddDays(-7));
+        }
+
+        [TestMethod]
+        public void NextShouldNext()
+        {
+            var controller = new CalendarController();
+            var today = DateTime.Today;
+
+            var result = (RedirectToRouteResult)controller.Prev(today);
+
+            Assert.AreEqual(result.RouteValues["FirstDay"], today.AddDays(-7));
+        }
+
+//        [TestMethod]
+//        public void OpeningAppointmentShouldPassID()
+//        {
+//            var controller = new CalendarController();
+//
+//
+//            var result = (RedirectToRouteResult)controller.Details();
+//
+//            Assert.AreEqual(result.RouteValues["FirstDay"], today.AddDays(-7));
+//        }
     }
 }
